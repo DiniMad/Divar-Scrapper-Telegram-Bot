ed74298afe6bfd2c6af713c0cf8894f25653e32c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-03-09 01:49:06 +0330

    Make some properties and override the str method to improve Car class.

--- Car.py
+++ Car.py
@@ -4,8 +4,25 @@ class Car:
         self.title = title
         self.price = price
         self.city = city
-        self.district = district
+        self.__district = district
         self.image_url = image_url
 
+    def __str__(self):
+        return f"[{self.title}]({self.link})\n\n[{self.address}]" \
+               f"({self.image_url})\n\n_{self.price}_"
+
+    @property
     def link(self):
         return f"https://divar.ir/v/{self.token}"
+
+    @property
+    def address(self):
+        return f"{self.city} - {self.district}"
+
+    @property
+    def district(self):
+        return self.__district if self.__district else ""
+
+    @district.setter
+    def district(self, value):
+        self._district = value
